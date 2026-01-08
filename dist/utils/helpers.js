d8a3ae07b7cd6be9715948aa00b9c24e56ffe5fd
white-fortune <whitefortune271828@gmail.com>
2026-01-08 21:40:06 +0600

    added hour shifts in event times via env config

--- dist/utils/helpers.js
+++ dist/utils/helpers.js
@@ -1,12 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.datetime = datetime;
+const secrets_1 = require("./secrets");
 /**
 * @param {number | string} ts
 * @description returns **datetime** in **mm/dd/yyyy** format, **time** in **HH:MM:SS AM/PM**
 */
 function datetime(ts) {
     const datetime = new Date(Number(ts) * 1000);
+    const timestampHourShift = (0, secrets_1.env)("TIMESTAMP_HOUR_SHIFT", "0");
+    if (timestampHourShift) {
+        datetime.setHours(datetime.getHours() + Number(timestampHourShift));
+    }
     return {
         datetime: datetime.toLocaleDateString(),
         day: datetime.toLocaleDateString("en-US", { weekday: "long" }),
